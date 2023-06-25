1e8fb9ea20b3f98a0c08087dce5824fbba7c2481
phil <phil.softwaredev@protonmail.com>
2023-06-25 15:50:26 +0200

    added launch script

---
+++ script.sh
@@ -0,0 +1,15 @@
+# pull the latest changes from your repository
+git pull
+
+# build the Docker image
+docker build -t curvemonitor .
+
+# stop and remove the existing container
+docker stop curvemonitor-container
+docker rm curvemonitor-container
+
+# run the new Docker image
+docker run --name curvemonitor-container --network some-network -p 3000:3000 -d curvemonitor
+
+# view the application logs
+docker logs -f curvemonitor-container
