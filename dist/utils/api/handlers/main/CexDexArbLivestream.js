462f53ab4e70ac08792104eef8c0a78b5113552c
phil <phil.softwaredev@protonmail.com>
2024-05-10 22:06:01 +0200

    muted matching binance trade spotting

--- dist/utils/api/handlers/main/CexDexArbLivestream.js
+++ dist/utils/api/handlers/main/CexDexArbLivestream.js
@@ -1,26 +1,25 @@
-import eventEmitter from "../../../goingLive/EventEmitter.js";
-import { enrichTransactionDetail } from "../../../postgresTables/readFunctions/TxDetailEnrichment.js";
-import { findBestMatchingBinanceTrade } from "../../../TokenPrices/txValue/binance/BinanceAPI.js";
-import { getEnrichedCexDexDetails } from "../../../postgresTables/mevDetection/cexdex/utils/cexdexDetection.js";
+import eventEmitter from '../../../goingLive/EventEmitter.js';
+import { enrichTransactionDetail } from '../../../postgresTables/readFunctions/TxDetailEnrichment.js';
+import { getEnrichedCexDexDetails } from '../../../postgresTables/mevDetection/cexdex/utils/cexdexDetection.js';
 export const handleCexDexArbLivestream = (mainRoom, socket) => {
     const newTransactionStreamHandler = async (txId) => {
         const enrichedTransaction = await enrichTransactionDetail(txId);
         if (enrichedTransaction) {
             const enrichedCexDexDetails = await getEnrichedCexDexDetails(enrichedTransaction);
             if (enrichedCexDexDetails) {
-                const bestMatchingBinanceTrade = await findBestMatchingBinanceTrade(enrichedTransaction);
+                // const bestMatchingBinanceTrade = await findBestMatchingBinanceTrade(enrichedTransaction);
                 // console.log("\nbest match for cexdexarb:", bestMatchingBinanceTrade, "for tx:", enrichedCexDexDetails);
-                mainRoom.in("CexDexArbLivestreamRoom").emit("NewCexDexArb", enrichedCexDexDetails, bestMatchingBinanceTrade);
+                mainRoom.in('CexDexArbLivestreamRoom').emit('NewCexDexArb', enrichedCexDexDetails, null);
             }
         }
     };
-    eventEmitter.on("New Transaction for CexDex-Arb-Livestream", newTransactionStreamHandler);
-    socket.on("connectToCexDexArbLivestream", async () => {
+    eventEmitter.on('New Transaction for CexDex-Arb-Livestream', newTransactionStreamHandler);
+    socket.on('connectToCexDexArbLivestream', async () => {
         console.log(`Client connected to CexDex-Arb-Livestream.`);
-        socket.join("CexDexArbLivestreamRoom");
+        socket.join('CexDexArbLivestreamRoom');
     });
-    socket.on("disconnect", () => {
-        eventEmitter.off("New Transaction for CexDex-Arb-Livestream", newTransactionStreamHandler);
+    socket.on('disconnect', () => {
+        eventEmitter.off('New Transaction for CexDex-Arb-Livestream', newTransactionStreamHandler);
         console.log(`Client disconnected.`);
     });
 };
--- src/utils/api/handlers/main/CexDexArbLivestream.ts
+++ src/utils/api/handlers/main/CexDexArbLivestream.ts
@@ -1,8 +1,8 @@
-import { Namespace, Socket } from "socket.io";
-import eventEmitter from "../../../goingLive/EventEmitter.js";
-import { enrichTransactionDetail } from "../../../postgresTables/readFunctions/TxDetailEnrichment.js";
-import { findBestMatchingBinanceTrade } from "../../../TokenPrices/txValue/binance/BinanceAPI.js";
-import { getEnrichedCexDexDetails } from "../../../postgresTables/mevDetection/cexdex/utils/cexdexDetection.js";
+import { Namespace, Socket } from 'socket.io';
+import eventEmitter from '../../../goingLive/EventEmitter.js';
+import { enrichTransactionDetail } from '../../../postgresTables/readFunctions/TxDetailEnrichment.js';
+import { findBestMatchingBinanceTrade } from '../../../TokenPrices/txValue/binance/BinanceAPI.js';
+import { getEnrichedCexDexDetails } from '../../../postgresTables/mevDetection/cexdex/utils/cexdexDetection.js';
 
 export const handleCexDexArbLivestream = (mainRoom: Namespace, socket: Socket) => {
   const newTransactionStreamHandler = async (txId: number) => {
@@ -11,22 +11,22 @@ export const handleCexDexArbLivestream = (mainRoom: Namespace, socket: Socket) =
     if (enrichedTransaction) {
       const enrichedCexDexDetails = await getEnrichedCexDexDetails(enrichedTransaction);
       if (enrichedCexDexDetails) {
-        const bestMatchingBinanceTrade = await findBestMatchingBinanceTrade(enrichedTransaction);
+        // const bestMatchingBinanceTrade = await findBestMatchingBinanceTrade(enrichedTransaction);
         // console.log("\nbest match for cexdexarb:", bestMatchingBinanceTrade, "for tx:", enrichedCexDexDetails);
-        mainRoom.in("CexDexArbLivestreamRoom").emit("NewCexDexArb", enrichedCexDexDetails, bestMatchingBinanceTrade);
+        mainRoom.in('CexDexArbLivestreamRoom').emit('NewCexDexArb', enrichedCexDexDetails, null);
       }
     }
   };
 
-  eventEmitter.on("New Transaction for CexDex-Arb-Livestream", newTransactionStreamHandler);
+  eventEmitter.on('New Transaction for CexDex-Arb-Livestream', newTransactionStreamHandler);
 
-  socket.on("connectToCexDexArbLivestream", async () => {
+  socket.on('connectToCexDexArbLivestream', async () => {
     console.log(`Client connected to CexDex-Arb-Livestream.`);
-    socket.join("CexDexArbLivestreamRoom");
+    socket.join('CexDexArbLivestreamRoom');
   });
 
-  socket.on("disconnect", () => {
-    eventEmitter.off("New Transaction for CexDex-Arb-Livestream", newTransactionStreamHandler);
+  socket.on('disconnect', () => {
+    eventEmitter.off('New Transaction for CexDex-Arb-Livestream', newTransactionStreamHandler);
     console.log(`Client disconnected.`);
   });
 };
